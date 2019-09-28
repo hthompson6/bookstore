019ddc2c86371da0070bb33f45f4c8773c8ff31b
Hunter Thompson <thompson.grey.hunter@gmail.com>
2019-09-28 22:48:05 +0000

    Added minikube

---
+++ kube/install_minikube_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CHECK_VIRT=$(egrep -c '(vmx|svm)' /proc/cpuinfo) 
+
+if [[ $CHECK_VIRT -le 0 ]]
+then
+    echo "ERROR: Virtualization Not Supported"
+    exit 1
+fi
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
