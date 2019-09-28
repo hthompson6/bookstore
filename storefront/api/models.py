ac4487fd0f9f457d14463306525cc7f4b3455c58
Hunter Thompson <thompson.grey.hunter@gmail.com>
2019-09-28 16:31:29 +0000

    Removed Entry model as only one data store is needed

--- storefront/api/models.py
+++ storefront/api/models.py
@@ -4,9 +4,3 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     pub_date = models.DateField()
-
-    class Meta:
-        abstract = True
-
-class Entry(models.Model):
-    book = models.EmbeddedModelField(model_container=Book,)
